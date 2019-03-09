8e9a0a560b13976315775b6377c097a81c902eca
Brian-Kwon <42950928+Brian-Kwon@users.noreply.github.com>
2019-03-10 05:07:17 +0900

    Update Untitled1.java

--- Untitled1.java
+++ Untitled1.java
@@ -3,6 +3,6 @@
  */
 public class Untitled1 {
 	public static void main(String[] args) {
-    	System.out.println("Hello from AWS Cloud9!");
+    	System.out.println("Hello from AWS Cloud9! It's not good for the Enterprise~");
 	}
 }
